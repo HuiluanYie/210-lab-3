7d3f08a7b8855918a83b4a5cba2f89d585c214ba
HuiluanYie <huiluanyie@gmail.com>
2026-09-03 00:00:23 -0700

    Commit 1

---
+++ main.cpp
@@ -0,0 +1,21 @@
+// COMSC-210 | Lab 3 | Huiluan Yie
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Restaurant
+{
+    string name;
+    int seating_capacity;
+    double rating;
+    bool offers_takeout;
+    double avg_price;
+};
+
+
+int main() {
+
+
+    return 0;
+}
